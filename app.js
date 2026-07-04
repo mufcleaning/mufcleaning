a65f57a63436837e3aa47930657e7055e7e5aaa1
mufcleaning <marwanaslam@gmail.com>
2026-07-04 16:18:42 +0500

    fix string formatting on line 29

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
   if (mode === 'subscribe' && token === verifyToken) {
-    console.log('WEBHOOK VERIFIED');
+  console.log('WEBHOOK VERIFIED');
     res.status(200).send(challenge);
   } else {
     res.status(403).end();
@@ -26,12 +26,12 @@ app.get('/', (req, res) => {
 // Route for POST requests
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(`\n\nWebhook received ${timestamp}\n`);
-  console.log(JSON.stringify(req.body, null, 2));
+console.log(`\n\nWebhook received ${timestamp}\n`);
+console.log(JSON.stringify(req.body, null, 2));
   res.status(200).end();
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`\nListening on port ${port}\n`);
+console.log(`\nListening on port ${port}\n`);
 });
